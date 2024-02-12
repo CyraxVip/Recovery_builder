7d8a8058bb65f921bdc3a96c047f3d34a239e796
CyraxVip <128605688+CyraxVip@users.noreply.github.com>
2024-02-12 15:54:53 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,14 +3,14 @@
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
 export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git" # the link of manifest
-export BRANCH="twrp-12.1" # the branch of manifest
+export BRANCH="twrp-11" # the branch of manifest
 
 # about your device
-export DEVICE="a21s" # codename used in device tree
-export DT_LINK="https://github.com/CyraxVip/android_device_samsung_a21s-1.git" # device tree link
+export DEVICE="selene" # codename used in device tree
+export DT_LINK="https://github.com/yazidkucrit/device_xiaomi_selene" # device tree link
 export DT_BRANCH="TWRP" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
-export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
+export VENDOR="xiaomi" # device manufacturer or vendor
+export TARGET="bootimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="twrp" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
